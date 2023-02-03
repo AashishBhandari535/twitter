0547bdd7a0e55851d41005cf242ea4a5866a9327
Aashish Bhandari <jhurenas@gmail.com>
2023-02-03 10:40:16 +0545

    add the post section of the feed component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,7 +1,30 @@
 import { SparklesIcon } from "@heroicons/react/24/solid";
 import Input from "./Input";
+import Post from "./Post";
 
 export default function Feed() {
+  const posts = [
+    {
+      id: "1",
+      name: "Sahand Ghavidel",
+      username: "codewithsahand",
+      userImg:
+        "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg",
+      img: "https://images.unsplash.com/photo-1500485035595-cbe6f645feb1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+      text: "nice-view!",
+      timestamp: "2 hours ago",
+    },
+    {
+      id: "2",
+      name: "Aashish Bhandari",
+      username: "codewithAsh",
+      userImg:
+        "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg",
+      img: "https://plus.unsplash.com/premium_photo-1664361480561-3bdcd6eb3b6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+      text: "nice-view!",
+      timestamp: "3 hours ago",
+    },
+  ];
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -11,6 +34,11 @@ export default function Feed() {
         </div>
       </div>
       <Input />
+      {
+        posts.map((post)=>(
+          <Post key={post.id} post={post}/>
+        ))
+      }
     </div>
   );
 }
